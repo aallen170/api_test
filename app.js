0fd7ee57343c224a787001e67d61a0142aaaf70f
Andrew Allen <1605707@TCS.com>
2019-06-12 16:13:06 -0500

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let query = 'tiger';
 let pageNum;
 
-let getPagesURL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=44f0f3e9f1bc34456bcf2b6df8499796&text=${query}&per_page=500&format=json&nojsoncallback=1`;
+let getPagesURL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=44f0f3e9f1bc34456bcf2b6df8499796&text=${query}&sort=relevance&per_page=500&format=json&nojsoncallback=1`;
 
 async function request(url) {
     const response = await fetch(url);
@@ -21,7 +21,7 @@ request(getPagesURL).then(data => {
 
     let percentageOfRelevance = 90;
 
-    let relevanceCalclation = pages * Math.abs(percentageOfRelevance / 100 - 1) - 1;
+    let relevanceCalclation = pages * (1 - percentageOfRelevance / 100) - 1;
 
     let randomNum = Math.floor(Math.random() * (relevanceCalclation)) + 1;
 
