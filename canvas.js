f6b75771d330941acca5c3e77042680703930ff4
Andrew Allen <1605707@TCS.com>
2019-06-20 16:22:20 -0500

    testing touch events for canvas.js

--- canvas.js
+++ canvas.js
@@ -3,15 +3,19 @@ dragElement(document.getElementById("obj"));
 
 //change
 
+document.addEventListener('touchstart', function() {
+  console.log("touching");
+});
+
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id)) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+    // document.getElementById(elmnt.id).onmousedown = dragMouseDown;
     document.getElementById(elmnt.id).ontouchstart = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV: 
-    elmnt.onmousedown = dragMouseDown;
+    // elmnt.onmousedown = dragMouseDown;
     elmnt.ontouchstart = dragMouseDown;
   }
 
@@ -21,10 +25,10 @@ function dragElement(elmnt) {
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
+    // document.onmouseup = closeDragElement;
     document.ontouchend = closeDragElement;
     // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
+    // document.onmousemove = elementDrag;
     document.ontouchmove = elementDrag;
   }
 
@@ -43,8 +47,8 @@ function dragElement(elmnt) {
 
   function closeDragElement() {
     // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
+    // document.onmouseup = null;
+    // document.onmousemove = null;
     document.ontouchend = null;
     document.ontouchmove = null;
   }
